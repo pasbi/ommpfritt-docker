c03d37062187d0363ddcb990bab1f1e13f812c6d
Pascal Bies <pascal.bies@k-lens.de>
2021-09-04 11:56:54 +0200

    build qtx11extras

--- scripts/install-qt6.sh
+++ scripts/install-qt6.sh
@@ -5,7 +5,7 @@ set -e
 git clone git://code.qt.io/qt/qt5.git
 cd qt5
 git checkout v6.0.4
-./init-repository --module-subset=qtbase,qtsvg,qttools,qttranslations,qtimageformats,qtshadertools,qtdeclarative,qtactiveqt
+./init-repository --module-subset=qtbase,qtsvg,qttools,qttranslations,qtimageformats,qtshadertools,qtdeclarative,qtactiveqt,qtx11extras
 cd ..
 mkdir build-qt6
 cd build-qt6
